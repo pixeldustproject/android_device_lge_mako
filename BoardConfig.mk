b0a9482ce103bb0260c6686788334d02a8d971bd
beanstown106 <nbenis106@gmail.com>
2016-11-23 15:43:59 -0500

    mako: enable night mode *seems google just likes to lie

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CONFIG := hellspawn_mako_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/mako
 TARGET_GCC_VERSION_ARM := 6.x
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
